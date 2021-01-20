cfd0db178dc76b1334ac923f5dbdf86f35adbd02
Ysarion <jeremy.daix92@gmail.com>
2021-01-20 14:03:28 +0100

    add cors without conf;

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import bodyParser from 'body-parser';
 import usersRoutes from './routes/users.js'
 import criteresRoutes from './routes/criteres.js'
 import articlesRoutes from './routes/articles.js'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/users', usersRoutes)
 app.use('/criteres', criteresRoutes)
