34290ae8de2a84e7af0d04893df2d22b6c589749
Ysarion <jeremy.daix92@gmail.com>
2021-01-20 14:25:25 +0100

    change codeArticles jsonObject to codesArticles to match the other side

---
+++ controllers/auditController.js
@@ -0,0 +1,4 @@
+const audits = [
+    {id:1,fk_user:1,fk_articles:1,four:"Four 1", numContainer: 150551, totalVerres:124, resultPourcentage:98, objectifAnnuel:97,rating:"A",commentaireGeneral: null, dateDeb:"2016-12-21 8:00:00.000", dateFin:"2016-12-21 16:00:00.000"},
+    {id:2,fk_user:1,fk_articles:1,four:"Four 1", numContainer: 145254, totalVerres:154, resultPourcentage:97, objectifAnnuel:97,rating:"B",commentaireGeneral: null, dateDeb:"2016-12-23 8:00:00.000", dateFin:"2016-12-23 16:00:00.000"},
+]
