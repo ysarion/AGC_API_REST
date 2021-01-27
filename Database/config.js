b7f6cfb22923aacfddf3ecec5abcefadb091f564
Ysarion <jeremy.daix92@gmail.com>
2021-01-27 15:09:56 +0100

    add config to establish connection to db + fetch data for usersController

---
+++ Database/config.js
@@ -0,0 +1,6 @@
+export const config = {
+    user: 'sa',
+    password: 'mySuperStrongP@ssword',
+    server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+    database: 'AGC_QUALITY'
+}
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,16 +1,36 @@
+import {config} from "../Database/config.js";
+import sql from 'mssql'
 const usersController = [
     {id: 1, firstName: 'jeremy', lastName: 'Daix', BE: 117059, role: 'User', equipe: null, tracabilite: null},
     {id: 2, firstName: 'Pierre', lastName: 'Faidherbe', BE: 117060, role: 'User', equipe: null, tracabilite: null}
 ]
 
 export const getUsers = (req, res) => {
-    //@todo retrieve data from db
-    res.send(usersController);
+    (async () => {
+        try {
+            await sql.connect(config)
+            let result = await sql.query('select * from Users')
+            res.send(result.recordset);
+        }catch (e){
+            console.log(e);
+        }
+    })()
+
 }
 
 export const getUser = (req, res) => {
     //@todo retrieve data from db
-    res.send(usersController[parseInt(req.params.id)]);
+    (async () => {
+        try {
+            await sql.connect(config)
+            let param = parseInt(req.params.id);
+            let result = await sql.query('select * from Users where idUser = '+param)
+            res.send(result.recordset);
+        }catch (err){
+            console.log(err);
+            res.send('L\'utilisateur recherché n\'existe pas en db');
+        }
+    })()
 }
 
 export const postUser = (req, res) => {
